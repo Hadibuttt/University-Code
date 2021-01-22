7f9fddb1ad5cc5202f0320a4475faa7154a6f5ed
Abdul Hadi Butt <hadibutt476@gmail.com>
2021-01-22 09:01:42 +0500

    Update Two-dimensional Array.cpp

--- Programming Fundamentals/Two-dimensional Array.cpp
+++ Programming Fundamentals/Two-dimensional Array.cpp
@@ -5,7 +5,7 @@ int main ()
     int scores[5][5], i, j, k, l;
     for(i=1;i<=5;i++)
     {
-            cout<<"Enter scores for the student "<<i<<endl;
+            cout<<"Enter scores for the player "<<i<<endl;
         for(j=1;j<=5;j++)
         {
             cin>>scores[i][j];
@@ -13,7 +13,7 @@ int main ()
     } 
     cout<<"Enter row number:";
     cin>>k;
-    cout<<"Enter student number in that row:";
+    cout<<"Enter player number in that row:";
     cin>>l;
     if(k<=5&&l<=5)
     cout<<"Score is "<<scores[k][l];
