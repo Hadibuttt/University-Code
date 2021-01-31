feaee8b1dc898339daa7456ab778dfeb0e83629b
Abdul Hadi Butt <hadibutt476@gmail.com>
2021-01-31 14:09:45 +0500

    Update Salary and grade.cpp

--- Programming Fundamentals/Salary and grade.cpp
+++ Programming Fundamentals/Salary and grade.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-	int salary,grade,bonus=1000;
+	int salary,grade,bonus=100;
 	cout<<"Enter Salary"<<"\n";
 	cin>>salary;
 	cout<<"Enter grade"<<"\n";
 	cin>>grade;
 	if(grade>=15)
 	{
-		salary=salary+(bonus/2);
+		salary=salary+(bonus/4);
 		cout<<"Salary is="<<salary<<"\n";
 	}
 	else if(grade<=15)
