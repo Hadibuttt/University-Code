7db3ddc7e9376f49dfa25c8ecf58c9718cc756f0
Abdul Hadi Butt <hadibutt476@gmail.com>
2021-09-29 01:37:17 +0500

    Intial Commit

---
+++ Visual Programming/CRUD Operations/CRUD Operations/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace CRUD_Operations
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtn = new System.Windows.Forms.TextBox();
+			this.txte = new System.Windows.Forms.TextBox();
+			this.create = new System.Windows.Forms.Button();
+			this.Update = new System.Windows.Forms.Button();
+			this.Read = new System.Windows.Forms.Button();
+			this.Delete = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip1.TabIndex = 0;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(38, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(38, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Email";
+			// 
+			// txtn
+			// 
+			this.txtn.Location = new System.Drawing.Point(115, 30);
+			this.txtn.Name = "txtn";
+			this.txtn.Size = new System.Drawing.Size(100, 20);
+			this.txtn.TabIndex = 3;
+			// 
+			// txte
+			// 
+			this.txte.Location = new System.Drawing.Point(115, 72);
+			this.txte.Name = "txte";
+			this.txte.Size = new System.Drawing.Size(100, 20);
+			this.txte.TabIndex = 4;
+			// 
+			// create
+			// 
+			this.create.Location = new System.Drawing.Point(254, 69);
+			this.create.Name = "create";
+			this.create.Size = new System.Drawing.Size(75, 23);
+			this.create.TabIndex = 5;
+			this.create.Text = "Create";
+			this.create.UseVisualStyleBackColor = true;
+			this.create.Click += new System.EventHandler(this.Create_Click);
+			// 
+			// Update
+			// 
+			this.Update.Location = new System.Drawing.Point(356, 70);
+			this.Update.Name = "Update";
+			this.Update.Size = new System.Drawing.Size(75, 23);
+			this.Update.TabIndex = 6;
+			this.Update.Text = "Update";
+			this.Update.UseVisualStyleBackColor = true;
+			this.Update.Click += new System.EventHandler(this.Update_Click);
+			// 
+			// Read
+			// 
+			this.Read.Location = new System.Drawing.Point(254, 118);
+			this.Read.Name = "Read";
+			this.Read.Size = new System.Drawing.Size(75, 23);
+			this.Read.TabIndex = 7;
+			this.Read.Text = "Read";
+			this.Read.UseVisualStyleBackColor = true;
+			this.Read.Click += new System.EventHandler(this.Read_Click);
+			// 
+			// Delete
+			// 
+			this.Delete.Location = new System.Drawing.Point(356, 118);
+			this.Delete.Name = "Delete";
+			this.Delete.Size = new System.Drawing.Size(75, 23);
+			this.Delete.TabIndex = 8;
+			this.Delete.Text = "Delete";
+			this.Delete.UseVisualStyleBackColor = true;
+			this.Delete.Click += new System.EventHandler(this.Delete_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Delete);
+			this.Controls.Add(this.Read);
+			this.Controls.Add(this.Update);
+			this.Controls.Add(this.create);
+			this.Controls.Add(this.txte);
+			this.Controls.Add(this.txtn);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.statusStrip1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtn;
+		private System.Windows.Forms.TextBox txte;
+		private System.Windows.Forms.Button create;
+		private System.Windows.Forms.Button Update;
+		private System.Windows.Forms.Button Read;
+		private System.Windows.Forms.Button Delete;
+	}
+}
+
---
+++ Visual Programming/CRUD Operations/CRUD Operations/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Operations
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Create_Click(object sender, EventArgs e)
+		{
+            string email = txte.Text;
+            string name = txtn.Text;
+            string query = "Insert into info (name,email) values('" + name + "','" + email + "')";
+            string con = "Data Source=DESKTOP-GS75OSF;Initial Catalog=User;Integrated Security=True";
+            SqlConnection sqlcon = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(query, sqlcon);
+            sqlcon.Open();
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                MessageBox.Show("Record Inserted Successfully");
+
+            }
+            else
+            {
+                MessageBox.Show("Error");
+
+            }
+            sqlcon.Close();
+
+        }
+
+		private void Update_Click(object sender, EventArgs e)
+		{
+            string email = txte.Text;
+            string name = txtn.Text;
+            string query = "UPDATE info SET name = '" + name + "' WHERE email = '" + email + "';";
+            string con = "Data Source=DESKTOP-GS75OSF;Initial Catalog=User;Integrated Security=True";
+            SqlConnection sqlcon = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(query, sqlcon);
+            sqlcon.Open();
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                MessageBox.Show("Record Updated Successfully");
+
+            }
+            else
+            {
+                MessageBox.Show("Error");
+
+            }
+            sqlcon.Close();
+
+        }
+
+		private void Read_Click(object sender, EventArgs e)
+		{
+            string email = txte.Text;
+            string name = txtn.Text;
+            string query = "Select * from info where email ='" + email + "'";
+            string con = "Data Source=DESKTOP-GS75OSF;Initial Catalog=User;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(con);
+            sqlCon.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlCon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            sqlCon.Close();
+            if (dt.Rows.Count > 0)
+            {
+                txtn.Text = dt.Rows[0]["name"].ToString();
+                txte.Text = dt.Rows[0]["email"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Record Not Found");
+            }
+        }
+
+		private void Delete_Click(object sender, EventArgs e)
+		{
+            string email = txte.Text;
+            string query = "DELETE FROM info WHERE email = '" + email +"';";
+            string con = "Data Source=DESKTOP-GS75OSF;Initial Catalog=User;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(query, sqlCon);
+            sqlCon.Open();
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                MessageBox.Show("Record Deleted Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            sqlCon.Close();
+        }
+	}
+}
